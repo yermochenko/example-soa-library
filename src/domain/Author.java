270137edcb6c9719fa28a423e98677df84fc45a3
yermochenko <yermochenko@gmail.com>
2018-10-01 22:34:07 +0300

    Domain was modeled

---
+++ src/domain/Author.java
@@ -0,0 +1,22 @@
+package domain;
+
+public class Author extends Entity {
+	private String firstName;
+	private String lastName;
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+}
---
+++ src/domain/Book.java
@@ -0,0 +1,22 @@
+package domain;
+
+public class Book extends Entity {
+	private Author author;
+	private String title;
+
+	public Author getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(Author author) {
+		this.author = author;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+}
