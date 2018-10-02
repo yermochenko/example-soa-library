7eed51abd2bdec7e39024a6dd3e12eb09f97ffd8
yermochenko <yermochenko@gmail.com>
2018-10-02 08:32:14 +0300

    Console test for method BookDao.readByTitle() was implemented.

---
+++ src/test/dao/BookDaoReadByTitleTest.java
@@ -0,0 +1,26 @@
+package test.dao;
+
+import java.util.List;
+
+import test.Utility;
+import dao.BookDao;
+import dao.fake.BookDaoFakeImpl;
+import domain.Book;
+
+public class BookDaoReadByTitleTest {
+	private static void output(BookDao bookDao, String title) {
+		List<Book> books = bookDao.readByTitle(title);
+		System.out.printf("Список всех книг, содержащих в названии строку «%s»\n", title);
+		System.out.println("====================================================");
+		for(Book book : books) {
+			System.out.println(Utility.toString(book));
+		}
+		System.out.println();
+	}
+
+	public static void main(String[] args) {
+		BookDao bookDao = new BookDaoFakeImpl();
+		output(bookDao, "книга");
+		output(bookDao, "синяя книга");
+	}
+}
